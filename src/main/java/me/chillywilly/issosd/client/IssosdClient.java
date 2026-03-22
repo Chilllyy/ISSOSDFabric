45a2b9b250a50b5542914cb26a20afb204aee96c
Chilllyy <chillywillymc@gmail.com>
2026-03-22 11:27:38 -0600

    update rendering to use new fabric API method

--- src/main/java/me/chillywilly/issosd/client/IssosdClient.java
+++ src/main/java/me/chillywilly/issosd/client/IssosdClient.java
@@ -8,11 +8,8 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.resources.Identifier;
@@ -24,12 +21,12 @@ public class IssosdClient implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("issosd");
     public static final ISSModConfig config = new ISSModConfig();
     LightstreamerClient client;
-    private float value = 0;
+    public float value = 0;
     public static IssosdClient instance;
-    public boolean animate = false;
-    public static Identifier texture;
     public static Identifier normal_texture = Identifier.fromNamespaceAndPath("issosd", "textures/gui/piss_icon.png");
     public static Identifier notif_texture = Identifier.fromNamespaceAndPath("issosd", "textures/gui/piss_icon_notif.png");
+
+    public SpacemanHudElement hudElement;
 
     @Override
     public void onInitializeClient() {
@@ -46,8 +43,8 @@ public class IssosdClient implements ClientModInitializer {
         client.subscribe(sub);
 
         sub.addListener(new ISSSubListener());
-        texture = IssosdClient.normal_texture;
-        HudElementRegistry.attachElementBefore(VanillaHudElements.CHAT, Identifier.fromNamespaceAndPath("issosd", "pissdisplay"), IssosdClient::render);
+        hudElement = new SpacemanHudElement(normal_texture);
+        HudElementRegistry.attachElementBefore(VanillaHudElements.CHAT, Identifier.fromNamespaceAndPath("issosd", "pissdisplay"), hudElement);
 
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(Commands.literal("issosd")
@@ -60,15 +57,6 @@ public class IssosdClient implements ClientModInitializer {
         int value = IntegerArgumentType.getInteger(context, "display_num");
         IssosdClient.instance.update(String.valueOf(value));
         return 1;
-    }
-
-    private static void render(GuiGraphics context, DeltaTracker counter) {
-        if (!config.getEnabled()) return;
-        int color = 0xFFA87132;
-        int x_start = (int) (context.guiWidth() * config.getX());
-        int y_start = (int) (context.guiHeight() * config.getY());
-        context.blit(RenderPipelines.GUI_TEXTURED, IssosdClient.texture, x_start, y_start, 0, 0, 16, 16, 16, 16);
-        context.drawString(Minecraft.getInstance().font, IssosdClient.instance.value + "%", x_start + 20, y_start + 4, color, true);
     }
 
     public void update(String newValue) {
@@ -89,9 +77,9 @@ public class IssosdClient implements ClientModInitializer {
                         if (val < old_value && checkDownSound()) {
                             playSoundToPlayer(config.getDownSound(), config.getDownSoundPitch());
                         }
-                        IssosdClient.texture = IssosdClient.notif_texture; //Display notification texture for 5 seconds
+                        hudElement.texture = notif_texture; //Display notification texture for 5 seconds
                         Thread.sleep(5000);
-                        IssosdClient.texture = IssosdClient.normal_texture;
+                        hudElement.texture = normal_texture;
                     } catch(InterruptedException ex) {
                         Thread.currentThread().interrupt();
                     }
---
+++ src/main/java/me/chillywilly/issosd/client/SpacemanHudElement.java
@@ -0,0 +1,26 @@
+package me.chillywilly.issosd.client;
+
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElement;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.resources.Identifier;
+
+public class SpacemanHudElement implements HudElement {
+    private ISSModConfig config;
+    public Identifier texture;
+    public SpacemanHudElement(Identifier texture) {
+        config = IssosdClient.config;
+    }
+    @Override
+    public void extractRenderState(GuiGraphicsExtractor guiGraphicsExtractor, DeltaTracker deltaTracker) {
+        if (!config.getEnabled()) return;
+        int color = 0xFFA87132;
+        int x_start = (int) (guiGraphicsExtractor.guiWidth() * config.getX());
+        int y_start = (int) (guiGraphicsExtractor.guiHeight() * config.getY());
+
+        guiGraphicsExtractor.blit(RenderPipelines.GUI_TEXTURED, texture, x_start, y_start, 0, 0, 16, 16, 16, 16, 16, 16, -1);
+        guiGraphicsExtractor.text(Minecraft.getInstance().font, IssosdClient.instance.value + "%", x_start + 20, y_start + 4, color, true);
+    }
+}
