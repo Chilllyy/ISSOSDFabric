cc875ebad1ccc664a6d8e4d3287aed4e6bfd3408
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2026-04-10 21:03:25 -0600

    switch from Lightstreamer to self hosted server

--- src/main/java/me/chillywilly/issosd/client/IssosdClient.java
+++ src/main/java/me/chillywilly/issosd/client/IssosdClient.java
@@ -1,10 +1,11 @@
 package me.chillywilly.issosd.client;
 
-import com.lightstreamer.client.LightstreamerClient;
-import com.lightstreamer.client.Subscription;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
@@ -14,35 +15,31 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.resources.Identifier;
 import net.minecraft.sounds.SoundEvent;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class IssosdClient implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("issosd");
     public static final ISSModConfig config = new ISSModConfig();
-    LightstreamerClient client;
-    public float value = 0;
+    public int value = 0;
     public static IssosdClient instance;
     public static Identifier normal_texture = Identifier.fromNamespaceAndPath("issosd", "textures/gui/piss_icon.png");
     public static Identifier notif_texture = Identifier.fromNamespaceAndPath("issosd", "textures/gui/piss_icon_notif.png");
 
     public SpacemanHudElement hudElement;
 
+    private Thread thread;
+
     @Override
     public void onInitializeClient() {
         config.load();
         IssosdClient.instance = this;
 
-        client = new LightstreamerClient("https://push.lightstreamer.com", "ISSLIVE");
-        client.connect();
-
-        String[] items = {"NODE3000005"};
-        String[] fields = {"Value"};
-        Subscription sub = new Subscription("MERGE", items, fields);
-        sub.setRequestedSnapshot("yes");
-        client.subscribe(sub);
-
-        sub.addListener(new ISSSubListener());
         hudElement = new SpacemanHudElement(normal_texture);
         HudElementRegistry.attachElementBefore(VanillaHudElements.CHAT, Identifier.fromNamespaceAndPath("issosd", "pissdisplay"), hudElement);
 
@@ -50,6 +47,14 @@ public class IssosdClient implements ClientModInitializer {
             dispatcher.register(Commands.literal("issosd")
                     .then(Commands.argument("display_num", IntegerArgumentType.integer())
                             .executes(IssosdClient::executeCommandWithArg)));
+        });
+
+        ServerFetcher fetcher = new ServerFetcher();
+        thread = new Thread(fetcher);
+        thread.start();
+
+        ClientLifecycleEvents.CLIENT_STOPPING.register((client) -> {
+            thread.interrupt();
         });
     }
 
@@ -61,33 +66,46 @@ public class IssosdClient implements ClientModInitializer {
 
     public void update(String newValue) {
         try {
-            float val = Float.parseFloat(newValue);
-            float old_value = value;
-            value = val;
-            LOGGER.info("Received New Value: {}", newValue);
-            new Thread() {
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(2000); //wait 2 seconds and check again
-                        if (val == old_value) return; //if value hasn't changed, skip all
-                        if (val > old_value && checkUpSound()) {
-                            playSoundToPlayer(config.getUpSound(), config.getUpSoundPitch());
-                        }
-                        if (val < old_value && checkDownSound()) {
-                            playSoundToPlayer(config.getDownSound(), config.getDownSoundPitch());
-                        }
-                        hudElement.texture = notif_texture; //Display notification texture for 5 seconds
-                        Thread.sleep(5000);
-                        hudElement.texture = normal_texture;
-                    } catch(InterruptedException ex) {
-                        Thread.currentThread().interrupt();
-                    }
-                }
-            }.start();
+            int val = Integer.parseInt(newValue);
+            update(val);
         } catch(NumberFormatException e) {
             LOGGER.warn("Number provided is not a number: {}", newValue);
         }
+    }
+
+    public void update(int newValue) {
+        if (value == newValue) return;
+        Identifier sound = null;
+        float pitch = 1.0F;
+        if (newValue > value) {
+            sound = config.getUpSound();
+            pitch = config.getUpSoundPitch();
+        } else {
+            sound = config.getDownSound();
+            pitch = config.getDownSoundPitch();
+        }
+        value = newValue;
+        LOGGER.debug("Received New Value: " + value);
+        playSoundToPlayer(sound, pitch);
+        new Thread(() -> {
+            try {
+                hudElement.texture = notif_texture;
+                Thread.sleep(5000);
+                hudElement.texture = normal_texture;
+            } catch (InterruptedException e) {
+                LOGGER.error("Thread interrupted during hud element change", e);
+            }
+        }).start();
+    }
+
+    public void update(JsonObject jsonObject) {
+        try {
+            float newValue = jsonObject.get("value").getAsFloat();
+            update((int)newValue);
+        } catch (NumberFormatException e) {
+            LOGGER.error("Value provided from webserver is not a number", e);
+        }
+
     }
 
     private void playSoundToPlayer(Identifier sound, float pitch) {
@@ -101,15 +119,42 @@ public class IssosdClient implements ClientModInitializer {
         player.playSound(SoundEvent.createVariableRangeEvent(sound), 1.0F, pitch);
     }
 
-    public boolean checkUpSound() {
-        return checkMod() && config.getUpSoundEnabled();
-    }
-
-    public boolean checkDownSound() {
-        return checkMod() && config.getDownSoundEnabled();
-    }
-
     public boolean checkMod() {
         return config.getEnabled();
+    }
+
+    private static class ServerFetcher implements Runnable {
+        private volatile boolean running = true;
+        private volatile int errorcount = 0;
+
+        @Override
+        public void run() {
+            LOGGER.info("Starting ISS Value Fetcher");
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(config.server_url)
+                    .build();
+            while (running && errorcount < 5) {
+                try {
+                    Response response = client.newCall(request).execute();
+                    if (response.code() == 200) {
+                        String json = response.body().string();
+                        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+                        IssosdClient.instance.update(jsonObject);
+                    } else {
+                        LOGGER.error(String.format("Webserver provided response code %s", response.code()));
+                        errorcount++;
+                    }
+                    Thread.sleep(30000); //Grab every 30 seconds
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    running = false;
+                } catch (IOException e) {
+                    LOGGER.error("Ran into issue while grabbing data from the web endpoint\n", e);
+                    errorcount++;
+                }
+            }
+            LOGGER.info("Stopping ISS Value Fetcher");
+        }
     }
 }
